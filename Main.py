e676856d7f9ef2154b94e845e138979b1bd35b1a
krloSc <carlos.vemartell@gmail.com>
2021-06-14 16:38:57 -0400

    Update

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 from Metaheuristics import *
-x=Simulated.Simulated_A()
-y=Simulated.Simulated_A()
-x.solution=2
-print(x.solution,y.solution)
+from problem.Problem import*
+
+problem=Problem("ground")
+parameters={"to":1000,"ta":0.001,"delta":0.99}
+x=Simulated.Simulated_A((4,3),parameters)
+solution=x.simulated(problem)
+print(solution)
--- Metaheuristics/Simulated.py
+++ Metaheuristics/Simulated.py
@@ -3,47 +3,45 @@ from solution.Solution import *
 import numpy as np
 from numpy.random import rand,uniform
 import matplotlib.pyplot as plt
+fit=Fitness()
+sol=Solution()
 
 class Simulated_A():
-    def __init__(self):
-        print(self)
+    def __init__(self,size,parameters):
+        self.solution=sol.init_solution(size[0],size[1])
+        self.parameters=parameters
+        print(self.solution)
 
-    def simulated(self,solution,n_sol,parameters):
-        t=parameters.get("to")
-        ta=parameters.get("ta")
-        delta=parameters.get("delta")
+    def simulated(self,problem):
+        t=self.parameters.get("to")
+        ta=self.parameters.get("ta")
+        delta=self.parameters.get("delta")
         n=1
         while t>ta:
             n_s=5
-            factor=uniform(-1,1,(n_s,solution.shape[1]))*1/(0.1*n)                                                           # neighbour (3,5,2)
-            neigbours=sol.generate_from2(solution,n_s,factor)
+            factor=uniform(-1,1,(n_s,self.solution.shape[1]))*1/(0.1*n)                                                           # neighbour (3,5,2)
+            neigbours=sol.generate_from2(self.solution,n_s,factor)
             #print(neigbours)
             for i in range(neigbours.shape[0]):
-                current_fitness=fit.evaluate(solution[i,:])
-                best_nbr=neigbours[i,np.argmin(fit.evaluate(neigbours[i,:,:]))]
-                if (fit.evaluate(best_nbr)<current_fitness):
-                    solution[i,:]=best_nbr
+                current_fitness=fit.evaluate(self.solution[i,:],problem)
+                best_nbr=neigbours[i,np.argmin(fit.evaluate(neigbours[i,:,:],problem))]
+                if (fit.evaluate(best_nbr,problem)<current_fitness):
+                    self.solution[i,:]=best_nbr
                 else:
                     r=rand()
-                    l=current_fitness-fit.evaluate(best_nbr)
+                    l=current_fitness-fit.evaluate(best_nbr,problem)
                     ann=np.exp(-l/t)
                     if (r<ann):
-                        solution[i,:]=best_nbr
+                        self.solution[i,:]=best_nbr
             t=t*delta
             n+=1
-        print(fit.evaluate(solution))
-        return solution
-
-    if __name__ == "__main__":
-       print("holis main")
-    print(__name__+"holis")
+        print(fit.evaluate(self.solution,problem))
+        return self.solution
 
 
-    fit=Fitness()
-    sol=Solution()
 
-    #Inicializacion
-    solution=sol.init_solution(10,2)                                                     #completar cambios de esta gente
+
+                                                     #completar cambios de esta gente
     """
 
     parameters={"to":1000,"ta":0.001,"delta":0.99}
